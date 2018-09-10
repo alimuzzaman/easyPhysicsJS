32b952e71825faa8f9a6f62f15f6ddea875ef0d0
Md. Alimuzzaman Alim <alim.alimuzzaman@usabilitydynamics.com>
2018-09-10 16:38:22 +0600

    Routing, animejs, bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import anime from 'animejs';
+import jQuery from 'jquery';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  imports: [NgbModule.forRoot()],
+  declarations: [],
+  bootstrap: []
+})
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'easyPhysics';
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+
+    var duration = anime({
+      targets: 'img',
+      translateX: {
+        value: 250,
+        duration: 800
+      },
+      rotate: {
+        value: 360,
+        duration: 1800,
+        easing: 'easeInOutSine'
+      },
+      scale: {
+        value: 2,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+      },
+      delay: 250
+    });
+
+  }
+
 }
